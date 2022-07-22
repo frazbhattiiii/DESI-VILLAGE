b545dc019d67668e3980d8997987e26f58062dbc
AsfandToor <toorasfand@gmailcom>
2022-07-23 01:41:03 +0500

    Vendor Updated

--- server/Controllers/vendorController.js
+++ server/Controllers/vendorController.js
@@ -1,6 +1,25 @@
 const Vendor = require('../Models/Vendor')
 const User = require('../Models/User')
 
+exports.getVendor = async (req, res) => {
+    const { vendor_id } = req.params
+    try {
+        const vendor = await Vendor.find({ _id: vendor_id })
+        res.status(200).json({
+            success: true,
+            message: "Vendor Fetched Successfully",
+            vendor
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "An unexpected error occured while fetching vendor",
+            error
+        })
+    }
+}
+
 exports.registerVendor = async (req, res) => {
     const { user_id, name, category, location, phone } = req.body
     const vendor = await Vendor.findOne({ name })
@@ -35,4 +54,12 @@ exports.registerVendor = async (req, res) => {
             }
         })
     }
+}
+
+exports.updateVendor = async (req, res) => {
+    const { vendor_id } = req.params
+    const newData = req.body
+    Object.keys(newData).forEach(props => {
+        
+    })
 }
--- server/Routes/vendorRoutes.js
+++ server/Routes/vendorRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express') 
 const router = express.Router()
-const { registerVendor } = require('../Controllers/vendorController')
+const { registerVendor, getVendor } = require('../Controllers/vendorController')
 
+router.get("/:vendor_id", getVendor)
 router.post("/register", registerVendor)
 
 module.exports = router
